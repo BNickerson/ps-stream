aaf70328adccbba2061f42fea184121f318d75dd
Blake Nickerson <blake.r.nickerson@gmail.com>
2018-09-21 03:12:28 +0000

    local changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = 80;
 
 app.use(express.static(__dirname + '/public'));
 console.log(__dirname + '/public');
--- public/script/socket-handling.js
+++ public/script/socket-handling.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000');
+var socket = io('http://45.76.25.17');
 socket.on('news', function (data) {
     $('#socket-message').html(data.message);
     $('#alert-wrapper').fadeIn(1500).delay(data.delay ? data.delay : 5000).fadeOut(1500);
