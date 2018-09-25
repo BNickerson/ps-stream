c97a31b1ed3d860d362b89288149498e8186696d
Blake Nickerson <blake.r.nickerson@gmail.com>
2018-09-24 20:22:28 -0500

    new stream server

--- main.js
+++ main.js
@@ -6,55 +6,15 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
-passport.use(new GoogleStrategy({
-    consumerKey: '',
-    consumerSecret: '',
-    callbackURL: 'http://www.powerspike.net/auth/google/callback'
-}, (token, tokenSecret, profile, done) => {
-    console.log(token);
-    console.log(tokenSecret);
-    console.log(profile);
-}));
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/powerspike', { useNewUrlParser: true });
-let db = mongoose.connection;
-
-db.on('error', (error) => {
-    console.log(error);
-});
-
-const User = require('./models/user');
-
 app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    User.find({}, (error, users) => {
-        if(error){
-            console.log(error);
-        } else {
-            res.render('index', {
-                title: 'Powerspike.net | Steelers Streams',
-                users: users
-            });
-        }
+    res.render('index', {
+        title: 'Powerspike.net | Steelers Streams',
     });
 });
-
-app.get('/auth/google', (req, res) => {
-    passport.authenticate('google', { scope: ['profile']});
-});
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log(req);
-    res.redirect('/');
-  });
 
 let connectCounter = 0;
 io.on('connection', (socket) => {
@@ -62,9 +22,6 @@ io.on('connection', (socket) => {
     io.sockets.emit('viewers', connectCounter);
     socket.on('send-news', (message) => {
         io.sockets.emit('news', (message));
-    });
-    socket.on('disconnect', (reason) => {
-        connectCounter--;
     });
 });
 
@@ -74,13 +31,13 @@ server.listen(Config.port, () => console.log(`Listening on port ${Config.port}`)
 
 // DISCORD CONNECTION
 
-const Discord = require('discord.js');
-let client = new Discord.Client();
+// const Discord = require('discord.js');
+// let client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Connected to Discord');
-    io.sockets.emit('discord-connected');
-});
+// client.on('ready', () => {
+//     console.log('Connected to Discord');
+//     io.sockets.emit('discord-connected');
+// });
 
 
-client.login(Config.discord.token);
+// client.login(Config.discord.token);
