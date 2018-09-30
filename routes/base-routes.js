b32e08c6ebc8b18d0ec0010ff808be9c98acc9ba
Blake Nickerson <blake.r.nickerson@gmail.com>
2018-09-30 18:28:03 -0500

    missed semicolon

--- routes/base-routes.js
+++ routes/base-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const fs = require('fs');
 
 router.get('/', (req, res) => {
-    let username = req.user ? req.user.username : null
+    let username = req.user ? req.user.username : null;
     res.render('index', {
         title: 'Powerspike.net | Steelers Streams',
         streamTitle: JSON.parse(fs.readFileSync('./data.json')).title,
