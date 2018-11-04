06ed3571731e2ef841a9f73f5e146537e8398114
Blake Nickerson <blake.r.nickerson@gmail.com>
2018-11-04 08:54:32 -0600

    added ability to close chat

--- public/script/main.js
+++ public/script/main.js
@@ -1,3 +1,12 @@
 $('#profileLink').click(function() {
     alert('Thanks for creating an account! Nothing exists here yet, but I have a lot planned!');
 });
+
+$('#close-chat').click(function() {
+    closeChat();
+});
+
+var closeChat = function() {
+    $('#player').removeClass('col-lg-8').addClass('col-lg-12');
+    $('#chat').addClass('hide');
+}
