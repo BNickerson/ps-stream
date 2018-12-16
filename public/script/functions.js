4ffe33069df610cbee80d3b6ccdc1246fac9c556
Blake Nickerson <BlakeN@buildabear.com>
2018-12-16 10:59:33 -0600

    remove old js

--- public/script/functions.js
+++ public/script/functions.js
@@ -4,14 +4,14 @@
         //***************************
         // FlexSlider Function
         //***************************
-        jQuery('.flexslider').flexslider({
-            animation: "slide",
-            prevText: "<em class='fa fa-angle-left'></em>",
-            nextText: "<em class='fa fa-angle-right'></em>",
-            start: function(slider) {
-                jQuery('body').removeClass('loading');
-            }
-        });
+        // jQuery('.flexslider').flexslider({
+        //     animation: "slide",
+        //     prevText: "<em class='fa fa-angle-left'></em>",
+        //     nextText: "<em class='fa fa-angle-right'></em>",
+        //     start: function(slider) {
+        //         jQuery('body').removeClass('loading');
+        //     }
+        // });
 
         //***************************
         // Sticky Header Function
@@ -59,34 +59,34 @@
         //***************************
         // PrettyPhoto Function
         //***************************
-        jQuery("area[rel^='prettyPhoto']").prettyPhoto();
+        // jQuery("area[rel^='prettyPhoto']").prettyPhoto();
 
-        jQuery(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({
-            animation_speed: 'normal',
-            theme: 'pp_default',
-            social_tools: '',
-            slideshow: 3000,
-            autoplay_slideshow: true
-        });
-        jQuery(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({
-            animation_speed: 'fast',
-            slideshow: 10000,
-            hideflash: true
-        });
+        // jQuery(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({
+        //     animation_speed: 'normal',
+        //     theme: 'pp_default',
+        //     social_tools: '',
+        //     slideshow: 3000,
+        //     autoplay_slideshow: true
+        // });
+        // jQuery(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({
+        //     animation_speed: 'fast',
+        //     slideshow: 10000,
+        //     hideflash: true
+        // });
 
-        jQuery("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
-            custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
-            changepicturecallback: function() {
-                initialize();
-            }
-        });
+        // jQuery("#custom_content a[rel^='prettyPhoto']:first").prettyPhoto({
+        //     custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
+        //     changepicturecallback: function() {
+        //         initialize();
+        //     }
+        // });
 
-        jQuery("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
-            custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
-            changepicturecallback: function() {
-                _bsap.exec();
-            }
-        });
+        // jQuery("#custom_content a[rel^='prettyPhoto']:last").prettyPhoto({
+        //     custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
+        //     changepicturecallback: function() {
+        //         _bsap.exec();
+        //     }
+        // });
         
         //***************************
         // SearchToggle Function
